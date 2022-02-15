746294521e4589f0aa8c07b53df7edc48fb93c1e
Javier García Escobedo <javiergarbedo@gmail.com>
2022-02-15 12:45:15 +0100

    Mejora diseño ficha

--- src/main/java/es/javiergarciaescobedo/cuatroenraya/Ficha.java
+++ src/main/java/es/javiergarciaescobedo/cuatroenraya/Ficha.java
@@ -1,17 +1,32 @@
 package es.javiergarciaescobedo.cuatroenraya;
 
-import java.util.Random;
+import javafx.scene.Group;
 import javafx.scene.paint.Color;
+import javafx.scene.paint.LinearGradient;
+import javafx.scene.shape.Arc;
 import javafx.scene.shape.Circle;
 
-public class Ficha extends Circle {
+public class Ficha extends Group {
         
     public Ficha() {
-        this.setRadius(20);
-        Random random = new Random();        
-        this.setFill(Color.rgb(random.nextInt(256), random.nextInt(256), random.nextInt(256)));
-        this.setCenterX(random.nextInt(640));
-        this.setCenterY(random.nextInt(480));
+        Circle circleExterior = new Circle();
+        circleExterior.setRadius(20);
+        circleExterior.setFill(Color.RED);
+        circleExterior.setCenterX(0);
+        circleExterior.setCenterY(0);
+        circleExterior.setStroke(Color.DARKGRAY);
+        
+        Circle circleInterior = new Circle();
+        circleInterior.setRadius(15);
+        LinearGradient g = LinearGradient.valueOf("from 0% 0% to 100% 100%, DarkRed 0%, Red 75% , White 100%");
+        circleInterior.setFill(g);
+        circleInterior.setCenterX(0);
+        circleInterior.setCenterY(0);
+        circleInterior.setStroke(Color.LIGHTGRAY);
+        
+        this.setLayoutX(100);
+        this.setLayoutY(100);
+        this.getChildren().addAll(circleExterior, circleInterior);
     }
     
 }
